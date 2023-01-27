919058fc8012851876b7eb5c98bd1ec47fecbf08
Fushko <fushkodev@outlook.it>
2023-01-27 17:34:41 +0100

    rewrite easing

--- include/easing.hpp
+++ include/easing.hpp
@@ -1,72 +1,54 @@
 #ifndef EASING_HPP
 #define EASING_HPP
 
+#include <functional>
 #include <chrono>
 #include <thread>
-#include <functional>
-#include <cmath>
+#include "utils.hpp"
 
-struct Animation
-{
-    double frame;
-	double time;
-	double duration_s;
-	int start;
-	int diff;
-	int fps;
-	Animation(int start, int end, int fps, int duration_ms);
+// Easing functions adapted from:
+// https://github.com/warrenm/AHEasing/blob/master/AHEasing/easing.c
+
+namespace easing {
+
+const auto ease_out_expo = [] (double t) {
+	return (t == 1.0) ? t : 1 - pow(2, -10 * t);
 };
 
-inline Animation::Animation(int start, int end, int fps, int duration_ms)
-{
-	this->start      = start;
-	this->diff       = end - start;
-	this->duration_s = duration_ms / 1000.;
-	this->fps   = fps;
-	this->frame = 1. / fps;
-	this->time  = 0.;
-}
-
-inline double ease_out_expo(double t, double b , double c, double d)
-{
-	return (t == d) ? b + c : c * (-pow(2, -10 * t / d) + 1) + b;
-}
-
-inline double ease_in_out_quad(double t, double b, double c, double d)
-{
-	if ((t /= d / 2) < 1)
-		return c / 2 * t * t + b;
+const auto ease_in_out_quad = [] (double t) {
+	if (t < 0.5)
+		return 2 * t * t;
 	else
-		return -c / 2 * ((t - 1) * (t - 3) - 1) + b;
-}
+		return (-2 * t * t) + (4 * t) - 1;
+};
 
-inline int ease_in_out_quad_loop(Animation a, int prev, int cur, int end, std::function<bool(int, int)> fn)
+void animate(std::function<double(double t)> easing, int start, int end, int duration_ms, int progress_ms, int prev_step, int cur_step, std::function<void(int)> fn, std::function<bool()> interrupt)
 {
-	if (!fn(cur, prev) || cur == end)
-		return cur;
+	while (true) {
+		if (cur_step == end) {
+			return;
+		}
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000 / a.fps));
+		prev_step = cur_step;
+		cur_step = lerp(start, end, easing(double(progress_ms) / duration_ms));
+		//printf("cur step %d, progress %d/%d\n", cur_step, progress_ms, duration_ms);
 
-	return ease_in_out_quad_loop(a,
-	    cur,
-	    int(ease_in_out_quad(a.time += a.frame, a.start, a.diff, a.duration_s)),
-	    end,
-	    fn);
+		if (prev_step != cur_step)
+			fn(cur_step);
+
+		if (progress_ms == duration_ms) {
+			return;
+		}
+
+		if (interrupt()) {
+			return;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		progress_ms += 1;
+	}
 }
 
-inline int ease_out_expo_loop(Animation a, int prev, int cur, int end, std::function<bool(int, int)> fn)
-{
-	if (!fn(cur, prev) || cur == end)
-		return cur;
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000 / a.fps));
-
-	return ease_out_expo_loop(a,
-	    cur,
-	    int(round(ease_out_expo(a.time += a.frame, a.start, a.diff, a.duration_s))),
-	    end,
-	    fn);
 }
-
 
 #endif // EASING_HPP
