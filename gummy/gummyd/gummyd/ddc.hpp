1082dc396e7f1aa52e322303fbcf28351e1bb1ee
Fushko <fushkodev@outlook.it>
2023-03-04 15:54:50 +0100

    add ddc functions

---
+++ gummy/gummyd/gummyd/ddc.hpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2021-2023, Francesco Fusco. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef DDC_HPP
+#define DDC_HPP
+
+#include <vector>
+#include <string>
+#include <ddcutil_c_api.h>
+#include <ddcutil_macros.h>
+#include <ddcutil_status_codes.h>
+
+namespace ddc {
+
+class display_refs;
+class info_list;
+
+class display {
+    DDCA_Display_Handle handle_;
+    DDCA_Feature_Metadata *info_;
+public:
+    display(DDCA_Display_Ref ref);
+    ~display();
+    DDCA_Display_Handle get() const;
+    DDCA_Non_Table_Vcp_Value get_brightness_vcp() const;
+    void set_brightness_vcp(uint16_t new_val);
+};
+
+// not guaranteed to be for brightness
+extern const DDCA_Vcp_Feature_Code brightness_code;
+std::string feature_name(DDCA_Vcp_Feature_Code vcp_code);
+std::vector<display> get_displays();
+
+} // namespace ddc
+
+#endif // DDC_HPP
