03c2742ba5346d57972c64e981b5f73602e6b8b1
Francesco Fusco <f.fusco@pm.me>
2024-01-09 19:31:05 +0100

    api refactor

--- gummyd/gummyd/api.hpp
+++ gummyd/gummyd/api.hpp
@@ -8,15 +8,28 @@
 #include <spdlog/spdlog.h>
 
 namespace gummyd {
+    // Get current configuration from disk.
+    nlohmann::json config_get();
+
+    // Write new configuration to disk. Daemon will read once started.
+    void config_write(nlohmann::json&);
+
+    // Returns false if it's already started.
     bool daemon_start();
+
+    // Returns false if it's already stopped.
     bool daemon_stop();
+
+    // Check if it's running.
     bool daemon_is_running();
-    void daemon_send(const std::string &s);
-    std::string daemon_get(std::string_view s);
 
-    nlohmann::json config_get_current();
-    void config_write(nlohmann::json);
+    // Get current settings for each screen.
+    nlohmann::json daemon_screen_status();
 
+    // Update daemon configuration.
+    void daemon_send_config(nlohmann::json&);
+
+    // Get min/max values for brightness and temperature.
     std::pair<int, int> brightness_range();
     std::pair<int, int> temperature_range();
 
