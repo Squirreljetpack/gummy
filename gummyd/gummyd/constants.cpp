ae517fdddcb820e1c0914ae210487465462b5720
Francesco Fusco <f.fusco@pm.me>
2024-01-21 01:09:17 +0100

    change fifo name

--- gummyd/gummyd/constants.cpp
+++ gummyd/gummyd/constants.cpp
@@ -7,7 +7,7 @@
 namespace gummyd {
 namespace constants {
 constexpr std::string_view flock_filename  = "gummyd-lock";
-constexpr std::string_view fifo_filename   = "gummy-fifo";
+constexpr std::string_view fifo_filename   = "gummyd-fifo";
 constexpr std::string_view config_filename = "gummyconf.json";
 constexpr int brt_steps_min  = 200;
 constexpr int brt_steps_max  = 1000;
